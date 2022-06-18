3725be30cf3ba7e0ac4fc92bb49d7d87fd39afe4
sachin <sachinkumarsingh51110@gmail.com>
2022-06-18 14:43:17 +0545

    layout improvement

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
+from flask_wtf import Form
+from wtforms import StringField,PasswordField,IntegerField,SubmitField,EmailField,validators
 app=Flask(__name__)
+
+
+class SignupForm(Form):
+    name=StringField("Name",[validators.DataRequired("Enter Your name"),validators.length(min=3)])
+    phone=IntegerField("Phone Number",[validators.DataRequired("Enter the Phone number"),validators.length(min=10,max=12)])
+    email=EmailField("Email",[validators.DataRequired("enter Your E-mail"),validators.length(min=5,max=90)])
+    submit=SubmitField("Submit")
+
+@app.route('/signup')
+def signup():
+    form=SignupForm()
+    return render_template("signup.html",form=form)    
 
 @app.route('/')
 def base():
@@ -18,4 +32,4 @@ def about():
     return render_template("about.html")
 
 if __name__=='__main__':
-    app.run(debug=true)    
+    app.run(debug=True)    
