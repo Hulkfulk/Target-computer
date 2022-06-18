d6044135a2e3828ba95947f4f443e9a7786ecd4c
sachin <sachinkumarsingh51110@gmail.com>
2022-06-18 08:05:44 +0545

    Basic app

---
+++ app.py
@@ -0,0 +1,23 @@
+import turtle
+from flask import Flask,render_template
+from sqlalchemy import true
+app=Flask(__name__)
+
+@app.route('/')
+def base():
+    return render_template("home.html")
+
+@app.route('/course')
+def course():
+    return render_template("courses.html") 
+
+@app.route('/price')
+def price():
+    return render_template("price.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+if __name__=='__main__':
+    app.run(debug=true)    
